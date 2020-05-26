0744f52ce5aaffba98b76def329bd75173745e39
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-26 15:56:07 +0700

    Add string resource to DuplicateUserException

--- app/src/main/java/id/my/avmmartin/goldexperience/exception/DuplicateUserException.java
+++ app/src/main/java/id/my/avmmartin/goldexperience/exception/DuplicateUserException.java
@@ -1,7 +1,9 @@
 package id.my.avmmartin.goldexperience.exception;
 
+import id.my.avmmartin.goldexperience.R;
+
 public class DuplicateUserException extends GeneralException {
     public DuplicateUserException() {
-        super("User not found");
+        super("Duplicate user", R.string.warning_email_duplicate);
     }
 }
